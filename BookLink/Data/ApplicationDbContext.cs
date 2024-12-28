b9cbd98d7012afccf2379c62317d4f6cd0948a6f
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2024-12-28 20:50:05 +0200

    Seed Category Table

--- BookLink/Data/ApplicationDbContext.cs
+++ BookLink/Data/ApplicationDbContext.cs
@@ -12,6 +12,19 @@ namespace BookLink.Data
 
 		public DbSet<Book> Books { get; set; }
 		public DbSet<Category> Categories { get; set; }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Category>().HasData(
+				new Category { CategoryId = 1, CategoryName = "Literature" },
+				new Category { CategoryId = 2, CategoryName = "History" },
+				new Category { CategoryId = 3, CategoryName = "Economics" },
+				new Category { CategoryId = 4, CategoryName = "Medicine & Health" },
+				new Category { CategoryId = 5, CategoryName = "Religions & Beliefs" },
+				new Category { CategoryId = 6, CategoryName = "Self-Development" },
+				new Category { CategoryId = 7, CategoryName = "Science Fiction & Fantasy" },
+				new Category { CategoryId = 8, CategoryName = "Philosophy" }
+				);
+		}
 
 	}
 }
---
+++ BookLink/Migrations/20241228184800_SeedCategoryTable.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookLink.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[,]
+                {
+                    { 1, "Literature" },
+                    { 2, "History" },
+                    { 3, "Economics" },
+                    { 4, "Medicine & Health" },
+                    { 5, "Religions & Beliefs" },
+                    { 6, "Self-Development" },
+                    { 7, "Science Fiction & Fantasy" },
+                    { 8, "Philosophy" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 8);
+        }
+    }
+}
