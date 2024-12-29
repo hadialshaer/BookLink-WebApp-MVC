92e9e79e4007ec2c293813d0c0a495c8e5b05f0a
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2024-12-29 20:00:47 +0200

    Customize category index view

--- BookLink/Migrations/20241228180436_AddCategoryTable.cs
+++ BookLink/Migrations/20241228180436_AddCategoryTable.cs
@@ -4,31 +4,31 @@
 
 namespace BookLink.Migrations
 {
-    /// <inheritdoc />
-    public partial class AddCategoryTable : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Categories",
-                columns: table => new
-                {
-                    CategoryId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
-                });
-        }
+	/// <inheritdoc />
+	public partial class AddCategoryTable : Migration
+	{
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.CreateTable(
+				name: "Categories",
+				columns: table => new
+				{
+					CategoryId = table.Column<int>(type: "int", nullable: false)
+						.Annotation("SqlServer:Identity", "1, 1"),
+					CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+				},
+				constraints: table =>
+				{
+					table.PrimaryKey("PK_Categories", x => x.CategoryId);
+				});
+		}
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "Categories");
-        }
-    }
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropTable(
+				name: "Categories");
+		}
+	}
 }
