e6aa9f47825ba8c102e757661f5bf1c63fca81b8
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2025-01-22 14:32:16 +0200

    refactor: add category interface with update and save methods

---
+++ BookLink.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using BookLink.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLink.DataAccess.Repository.IRepository
+{
+	public interface ICategoryRepository : IRepository<Category> // Category is a model
+	{
+		void Update(Category category);
+		void Save();
+		
+	}
+}
