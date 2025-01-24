7985a175662486e78f87987eb58bc8324d3f88ad
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2025-01-24 16:41:01 +0200

    Add Book and Author Model

---
+++ BookLink.Models/Author.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLink.Models
+{
+	public class Author
+	{
+		[BindNever]
+		public int AuthorId { get; set; }
+
+		[Required]
+		[StringLength(50, ErrorMessage = "this name is not allowed", MinimumLength = 2)]
+		public string FirstName { get; set; }
+
+		[Required]
+		[StringLength(50, ErrorMessage = "this last name is not allowed", MinimumLength = 2)]
+		public string LastName { get; set; }
+
+		public virtual IEnumerable<Book> Books { get; set; }
+
+	}
+}
--- BookLink.Models/Book.cs
+++ BookLink.Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BookLink.Models;
 
@@ -17,15 +18,58 @@ public class Book
 	[StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.", MinimumLength = 5)]
 	public string Description { get; set; }
 
+
 	[Required]
-	public string CoverImageUrl { get; set; }
+	[Display(Name ="List Price")]
+	[Range(1,100, ErrorMessage = "Price must be between $1 and $100")]
+	public double ListPrice { get; set; }
 
-	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-	public DateTime? UpdatedAt { get; set; }
+	[Required]
+	[Display(Name = "Price for 1-3")]
+	[Range(1, 1000, ErrorMessage = "Price must be between $1 and $1000")]
+	public double Price { get; set; }
 
-	[BindNever]
-	[Display(Name = "Available")]
-	public bool IsAvailable { get; set; } = true; // Default to available
+	[Required]
+	[Display(Name = "Price for 3+")]
+	[Range(1, 100, ErrorMessage = "Price must be between $1 and $100")]
+	public double Price3 { get; set; }
+
+	[Required]
+	[Display(Name = "Price for 5+")]
+	[Range(1, 100, ErrorMessage = "Price must be between $1 and $100")]
+	public double Price5 { get; set; }
+
+
+	// Relations
+	[Required]
+	[ForeignKey(nameof(Author))]
+	public int AuthorId { get; set; }
+	public virtual Author Author { get; set; }
+
+
+	//[Required]
+	//public string CoverImageUrl { get; set; }
+
+	//public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+	//public DateTime? UpdatedAt { get; set; }
+
+	//[BindNever]
+	//[Display(Name = "Available")]
+	//public bool IsAvailable { get; set; } = true; // Default to available
+
+
+	/// ///////////////////////////////////////////////////////////////////////////////
+	// Borrowing Details
+	//public DateTime? BorrowedDate { get; set; } // When it was borrowed
+	//public DateTime? DueDate { get; set; } // When it should be returned
+	//////////////////////////////////////////////////////////////////////////////
+	//[Required]
+	//public bool IsForBorrow { get; set; } // True if available for borrowing
+
+	//[Required]
+	//public bool IsForSale { get; set; } // True if available for sale	
+
+	//public string Condition { get; set; } // New, Good, Used, Damaged
 
 }
 
