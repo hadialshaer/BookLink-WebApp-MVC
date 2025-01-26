eaa9fc2a835526a58c0559e6b5e369cdbc220f96
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2025-01-26 20:56:54 +0200

    feat: Load Book DataTable

---
+++ BookLink/wwwroot/js/book.js
@@ -0,0 +1,19 @@
+﻿
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+
+    dataTable = $('#tableData').DataTable({
+        "ajax": { url: '/admin/book/getall' },
+        "columns": [
+            { data: 'title', "width": "25%" },
+            { data: 'listPrice', "width": "15%" },
+            { data: 'author', "width": "20%" },
+            { data: 'bookCategory.categoryName', "width": "15%" }
+            //{ data: 'Actions', "width": "25%" }
+        ]
+    });
+
+}
