9fa11c267b6fc09e9fcff98ff75fea7499801d3a
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2025-01-26 22:33:14 +0200

    feat: use SweatAlerts for delete Book

--- BookLink/wwwroot/js/book.js
+++ BookLink/wwwroot/js/book.js
@@ -1,4 +1,6 @@
-﻿$(document).ready(function () {
+﻿var dataTable;
+
+$(document).ready(function () {
     loadDataTable();
 });
 
@@ -6,19 +8,19 @@ function loadDataTable() {
     dataTable = $('#tableData').DataTable({
         "ajax": { url: '/admin/book/getall' }, // Fetches data from your API endpoint
         "columns": [
-            { data: 'title', "width": "25%" },
-            { data: 'listPrice', "width": "15%" },
-            { data: 'author', "width": "20%" },
-            { data: 'bookCategory.categoryName', "width": "15%" },
+            { data: 'title', "width": "25%", "className": "text-center" },
+            { data: 'listPrice', "width": "15%", "className": "text-center" },
+            { data: 'author', "width": "20%", "className": "text-center" },
+            { data: 'bookCategory.categoryName', "width": "15%", "className": "text-center" },
             {
                 data: 'bookId', // The unique ID for each book
                 "render": function (data) {
                     return `
                         <div class="d-flex justify-content-center">
-                            <a href="/Admin/Book/Upsert?id=${data}" class="btn btn-primary"">
+                            <a href="/Admin/Book/Upsert?id=${data}" class="btn btn-primary">
                                 <i class="bi bi-pencil-square"></i> Edit
                             </a>
-                            <a href="/Admin/Book/Delete/${data}" class="btn btn btn-danger">
+                            <button onClick=Delete("/Admin/Book/Delete/${data}") class="btn btn btn-danger mx-1">
                                 <i class="bi bi-trash"></i> Delete
                             </button>
                         </div>`
@@ -26,5 +28,28 @@ function loadDataTable() {
                 "width": "25%"
             }
         ]
+    });
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: "DELETE",
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message);
+                }
+            })
+        }
     });
 }
