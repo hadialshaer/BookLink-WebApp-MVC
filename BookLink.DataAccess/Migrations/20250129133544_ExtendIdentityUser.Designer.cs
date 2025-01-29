8f05118102ce36fe827f9999b203abcbc7844695
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2025-01-29 15:41:29 +0200

    feat: extend IdentityUser Table

--- BookLink.DataAccess/Migrations/20250129133544_ExtendIdentityUser.Designer.cs
+++ BookLink.DataAccess/Migrations/20250129133544_ExtendIdentityUser.Designer.cs
@@ -4,6 +4,7 @@ using BookLink.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookLink.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250129133544_ExtendIdentityUser")]
+    partial class ExtendIdentityUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -426,9 +429,6 @@ namespace BookLink.DataAccess.Migrations
 
                     b.Property<DateTime>("BirthDate")
                         .HasColumnType("datetime2");
-
-                    b.Property<string>("City")
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<DateTime>("CreatedAt")
                         .HasColumnType("datetime2");
---
+++ BookLink.Models/User.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLink.Models
+{
+	public class User : IdentityUser
+	{
+
+		[Required]
+		[StringLength(50, ErrorMessage = "This name is not allowed", MinimumLength = 3)]
+		public string FirstName { get; set; }
+
+		[Required]
+		[StringLength(100, ErrorMessage = "This last name is not allowed", MinimumLength = 4)]
+		public string LastName { get; set; }
+
+		public string? City { get; set; }
+
+		[DataType(DataType.Date)]
+		public DateTime BirthDate { get; set; }
+		public int age { get; set; }
+
+		public Gender gender { get; set; }
+
+		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+	}
+
+	public enum Gender{
+		Male,
+		Female
+	}
+}
