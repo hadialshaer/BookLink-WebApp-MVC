73e50550d4426cab92b809b23c978eec21053e50
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2025-01-29 14:15:34 +0200

    fix: Delete the new ApplicationDbContext added by Identity

--- BookLink.DataAccess/Data/ApplicationDbContext.cs
+++ BookLink.DataAccess/Data/ApplicationDbContext.cs
@@ -1,11 +1,12 @@
 ﻿using BookLink.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 
 namespace BookLink.DataAccess.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 {
 	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
 	{
