888008d74bcccb17d89832014f9feeb9e93db729
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2025-02-06 22:59:07 +0200

    refactor: add new static data for Status

--- BookLink.Utility/SD.cs
+++ BookLink.Utility/SD.cs
@@ -8,9 +8,24 @@ namespace BookLink.Utility
 {
 	public static class SD
 	{
+		// Roles
 		public const string Role_Admin = "Admin";
 		public const string Role_Member = "Member";
 		public const string Role_Guest = "Guest";
+
+
+		// Order Status
+		public const string StatusPending = "Pending";
+		public const string StatusApproved = "Approved";
+		public const string StatusRejected = "Processing";
+		public const string StatusShipped = "Shipped";
+		public const string StatusCancelled = "Cancelled";
+		public const string StatusRefunded = "Refunded";
+
+		// Payment Status
+		public const string PaymentStatusPending = "Pending";
+		public const string PaymentStatusApproved = "Approved";
+		public const string PaymentStatusRejected = "Rejected";
 
 	}
 }
