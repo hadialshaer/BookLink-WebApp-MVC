28fae906cbb5ad13a429f9dfd889c7da7718af80
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2025-02-06 10:02:02 +0200

    feat: implement UI for ShoppingCart

---
+++ BookLink/Areas/Member/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookLink.Areas.Member.Controllers
+{
+	public class CartController : Controller
+	{
+		[Area("Member")]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
