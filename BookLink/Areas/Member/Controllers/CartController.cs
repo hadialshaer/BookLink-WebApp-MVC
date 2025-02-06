8ef9eabb96b76881a6a28ca3f18d1015cc7c0a0f
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2025-02-06 19:44:57 +0200

    feat: Order UI

--- BookLink/Areas/Member/Controllers/CartController.cs
+++ BookLink/Areas/Member/Controllers/CartController.cs
@@ -42,6 +42,11 @@ namespace BookLink.Areas.Member.Controllers
 			return View(ShoppingCartVM);
 		}
 
+		public IActionResult CheckOut()
+		{
+			return View();
+		}
+
 		public IActionResult Plus(int cartId)
 		{
 			var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
