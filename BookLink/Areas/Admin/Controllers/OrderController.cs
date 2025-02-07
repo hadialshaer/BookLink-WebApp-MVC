7f9eebdfc9a2a54c28ad8df01b388a38952acb05
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2025-02-07 19:19:49 +0200

    feat: implement OrderController and Add OrderVM

---
+++ BookLink/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using BookLink.DataAccess.Repository.IRepository;
+using BookLink.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookLink.Areas.Admin.Controllers
+{
+	public class OrderController : Controller
+	{
+
+		private readonly IUnitOfWork _unitOfWork;
+		public OrderController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API CALLS
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "User").ToList();
+
+			return Json(new { data = objOrderHeaders });
+		}
+		}
+		#endregion
+	}
+}
