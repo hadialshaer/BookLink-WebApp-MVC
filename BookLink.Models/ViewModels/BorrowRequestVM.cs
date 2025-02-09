1462df425cae17723491200c42cb3f694990537c
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2025-02-09 16:05:06 +0200

    feat: Add BorrowRequest View Model

---
+++ BookLink.Models/ViewModels/BorrowRequestVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLink.Models.ViewModels
+{
+	public class BorrowRequestVM
+	{
+		public BorrowRequest BorrowRequest { get; set; }
+		public Book Book { get; set; }
+		public User Lender { get; set; }
+		public User Borrower { get; set; }
+	}
+}
