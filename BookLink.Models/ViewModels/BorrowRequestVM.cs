5c0723aad5f3c36eb555d61abcd4a7d5052e9a42
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2025-02-11 20:24:31 +0200

    feat: Update BorrowRequestVM

--- BookLink.Models/ViewModels/BorrowRequestVM.cs
+++ BookLink.Models/ViewModels/BorrowRequestVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +10,7 @@ namespace BookLink.Models.ViewModels
 	public class BorrowRequestVM
 	{
 		public BorrowRequest BorrowRequest { get; set; }
-		public Book Book { get; set; }
-		public User Lender { get; set; }
-		public User Borrower { get; set; }
+
+		public IEnumerable<SelectListItem> Loactions { get; set; }
 	}
 }
