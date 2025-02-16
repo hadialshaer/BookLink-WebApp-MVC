8f4ec4fd569708982124dc2def62be846f9364cd
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2025-02-16 16:27:05 +0200

    refactor: Add ShoppingCartViewComponent backend Logic

--- BookLink/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ BookLink/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -26,7 +26,6 @@ namespace BookLink.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
-            HttpContext.Session.Clear();
 			await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
---
+++ BookLink/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,43 @@
+﻿using BookLink.DataAccess.Repository.IRepository;
+using BookLink.Models;
+using BookLink.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookLink.ViewComponents
+{
+	public class ShoppingCartViewComponent : ViewComponent
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		private readonly UserManager<User> _userManager;
+
+		public ShoppingCartViewComponent(IUnitOfWork unitOfWork, UserManager<User> userManager)
+		{
+			_unitOfWork = unitOfWork;
+			_userManager = userManager;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var count = HttpContext.Session.GetInt32(SD.SessionCart);
+			var userId = _userManager.GetUserId((System.Security.Claims.ClaimsPrincipal)User);
+
+			if (userId != null)
+			{
+				if (count == null)
+				{
+					HttpContext.Session.SetInt32(SD.SessionCart,
+					_unitOfWork.ShoppingCart.GetAll(u => u.UserId == userId).Count());
+				}
+				return View(count);
+
+			}
+			else
+			{
+				HttpContext.Session.Clear();
+				return View(0);
+			}
+
+		}
+	}
+}
